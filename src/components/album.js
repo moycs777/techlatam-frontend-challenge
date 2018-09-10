5e7ca045423de24acd904ee970e6c72880d884e0
moycs777 <moycs777@gmail.com>
2018-09-10 09:23:53 -0500

    adding api call

--- src/components/album.js
+++ src/components/album.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 
 
+const api = 'https://itunes.apple.com/us/rss/topalbums/limit=100/json';
+
 class Album extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            entries:[],
+            isLoading:false,
+            error:false
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading:true });
+        fetch(api)
+            .then( res => {
+                if (res.ok) {
+                    return res.json();
+                }else{
+                    throw new Error("something went wrong");
+                }
+            })
+            .then( (data) => {
+                console.log("api data: " + JSON.stringify(data));
+                this.setState({ isLoading:false, entries:data.feed.entry})
+            })
+            .catch(err=> this.setState({isLoading:false}));
+    } 
+
     render() {
+        const { entries, isLoading, error } = this.state;
         return (
             <div className="App">
                 <p>Album Component</p>
