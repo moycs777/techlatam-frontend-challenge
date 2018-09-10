1c257b6cf1fbf5a99bf46a3d06ac22f06649772a
moycs777 <moycs777@gmail.com>
2018-09-10 09:33:11 -0500

    displaying the albums

--- src/components/album.js
+++ src/components/album.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-
+// url form itunes api
 const api = 'https://itunes.apple.com/us/rss/topalbums/limit=100/json';
 
 class Album extends Component {
@@ -8,6 +8,7 @@ class Album extends Component {
     constructor(props) {
         super(props);
 
+        // variables needed for the app
         this.state = {
             entries:[],
             isLoading:false,
@@ -16,6 +17,7 @@ class Album extends Component {
     }
 
     componentDidMount() {
+        // using loading text
         this.setState({ isLoading:true });
         fetch(api)
             .then( res => {
@@ -26,20 +28,35 @@ class Album extends Component {
                 }
             })
             .then( (data) => {
-                console.log("api data: " + JSON.stringify(data));
+                // the calling is done so we cancel the loading text
+                console.log("api data: " + JSON.stringify(data.feed.entry[1]));
                 this.setState({ isLoading:false, entries:data.feed.entry})
             })
             .catch(err=> this.setState({isLoading:false}));
     } 
-
     render() {
         const { entries, isLoading, error } = this.state;
+        if (error) {
+            return <p>{error.message}</p>;    
+        }
+        if (isLoading) {
+            return <p>Loading ...</p>;
+        }
         return (
-            <div className="App">
-                <p>Album Component</p>
+            <div>
+                <h1>albums</h1>
+                <ul>
+                    {entries.map(album =>
+                        <li key={album.title.label}>
+                            <a href={album.link}>{album.title.label}</a>
+                        </li>
+                    )}
+                </ul>
             </div>
         );
     }
+
+    
 }
 
 export default Album;
