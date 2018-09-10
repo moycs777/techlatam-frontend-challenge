2893cd91991927a920878a7aa80d3cc223f54ea4
moycs777 <moycs777@gmail.com>
2018-09-10 09:07:09 -0500

    album component created

---
+++ src/components/album.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Album extends Component {
+    render() {
+        return (
+            <div className="App">
+                <p>Album Component</p>
+            </div>
+        );
+    }
+}
+
+export default Album;
